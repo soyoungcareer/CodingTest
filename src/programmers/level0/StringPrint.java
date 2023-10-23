eb9174cbee07bb30c97f8cdb72e593cbadf751f8
soyoungcareer <soyoungcareer@gmail.com>
2023-10-23 21:24:34 +0900

    [프로그래머스] Lv0 > 문자열 출력하기

---
+++ src/programmers/level0/StringPrint.java
@@ -0,0 +1,22 @@
+package programmers.level0;
+
+import java.util.Scanner;
+
+/*
+ * 2023-10-23 (월)
+ * 
+ * 프로그래머스 > Java > Lv.0 > 문자열 출력하기
+ * 
+ * (문제)
+ * 문자열 str이 주어질 때, str을 출력하는 코드를 작성해 보세요.
+ * 
+ */
+
+public class StringPrint {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+
+        System.out.println(a);
+    }
+}
