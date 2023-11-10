074ab5c0762de9102012040b75f09646462eab6f
Soyoung Jeong <76424733+soyoungcareer@users.noreply.github.com>
2023-11-10 16:06:30 +0900

    [level unrated] Title: 간단한 논리 연산, Time: 0.01 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181917. 간단한 논리 연산/간단한 논리 연산.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean solution(boolean x1, boolean x2, boolean x3, boolean x4) {
+        boolean answer = true;
+        boolean first = vFunc(x1, x2);
+        boolean second = vFunc(x3, x4);
+
+        answer = (first == second) ? first : false;
+
+        return answer;
+    }
+
+    public boolean vFunc(boolean a, boolean b) {
+        boolean rslt = (a == b) ? a : true;
+        return rslt;
+    }
+}
