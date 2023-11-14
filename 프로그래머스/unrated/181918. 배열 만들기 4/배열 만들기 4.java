60f89a179e0e811de5383a1b353787dd9b0edab7
Soyoung Jeong <76424733+soyoungcareer@users.noreply.github.com>
2023-11-14 12:17:44 +0900

    [level unrated] Title: 배열 만들기 4, Time: 3.59 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181918. 배열 만들기 4/배열 만들기 4.java
+++ 프로그래머스/unrated/181918. 배열 만들기 4/배열 만들기 4.java
@@ -1,8 +1,30 @@
+import java.util.Stack;
 import java.util.ArrayList;
 import java.util.List;
 
 class Solution {
     public int[] solution(int[] arr) {
+        /* 스택 풀이 */
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            if (stack.empty()) {
+                stack.push(arr[i]);
+            } else if (stack.peek() < arr[i]) {
+                stack.push(arr[i]);
+            } else {
+                stack.pop();
+                i--;
+            }
+        }
+
+        int[] stk = new int[stack.size()];
+        for (int i = 0; i < stk.length; i++) {
+            stk[i] = stack.get(i);
+        }
+
+
+        /* 기존 풀이
         int[] stk = {};
         List<Integer> list = new ArrayList<Integer>();
 
@@ -21,6 +43,7 @@ class Solution {
         }
 
         stk = list.stream().mapToInt(i -> i).toArray();
+         */
 
         return stk;
     }
