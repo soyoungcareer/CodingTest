69fce13c1721d69ffc5d721398c79be193fe7c8a
Soyoung Jeong <76424733+soyoungcareer@users.noreply.github.com>
2023-11-16 08:05:30 +0900

    [level unrated] Title: 수열과 구간 쿼리 3, Time: 0.03 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.java
+++ 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.java
@@ -1,6 +1,14 @@
 class Solution {
     public int[] solution(int[] arr, int[][] queries) {
-        int[] answer = {};
-        return answer;
+        for (int i = 0; i < queries.length; i++) {
+            int frontIdx = queries[i][0];
+            int backIdx = queries[i][1];
+
+            int temp = arr[frontIdx];
+            arr[frontIdx] = arr[backIdx];
+            arr[backIdx] = temp;
+        }
+
+        return arr;
     }
 }
