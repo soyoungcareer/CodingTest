2317631de74b6bc3b147175cb90ceaa5e266b5bc
Soyoung Jeong <76424733+soyoungcareer@users.noreply.github.com>
2023-11-17 12:20:23 +0900

    [level unrated] Title: 배열 만들기 5, Time: 8.28 ms, Memory: 80.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181912. 배열 만들기 5/배열 만들기 5.java
+++ 프로그래머스/unrated/181912. 배열 만들기 5/배열 만들기 5.java
@@ -9,11 +9,16 @@ class Solution {
             int num = Integer.parseInt(i.substring(s, s + l));
             if (num > k ) list.add(num);
         }
+        
+        // stream으로 처리
+        int[] answer = list.stream().mapToInt(Integer::intValue).toArray();
 
+        /* (기존 풀이) - 반복문으로 list를 배열에 담음.
         int[] answer = new int[list.size()];
         for (int i = 0; i < list.size(); i++) {
             answer[i] = list.get(i);
         }
+        */
 
         return answer;
     }
