3890209e324f0328965697e498d3fa4b7636b833
Soyoung Jeong <76424733+soyoungcareer@users.noreply.github.com>
2023-11-20 12:09:00 +0900

    [level unrated] Title: 접미사인지 확인하기, Time: 0.03 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181908. 접미사인지 확인하기/접미사인지 확인하기.java
+++ 프로그래머스/unrated/181908. 접미사인지 확인하기/접미사인지 확인하기.java
@@ -1,5 +1,9 @@
 class Solution {
     public int solution(String my_string, String is_suffix) {
+        /* endsWith 메소드 활용 */
+        int answer = my_string.endsWith(is_suffix) ? 1 : 0;
+        
+        /* (기존 풀이)
         int answer = 0;
         int myStrLen = my_string.length();
         int suffLen = is_suffix.length();
@@ -9,6 +13,7 @@ class Solution {
         } else {
             answer = is_suffix.equals(my_string.substring(myStrLen - suffLen)) ? 1 : 0;
         }
+        */
         
         return answer;
     }
