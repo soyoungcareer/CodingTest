531544f34c55868f9bafac132f377060bbc4c677
Soyoung Jeong <76424733+soyoungcareer@users.noreply.github.com>
2023-11-23 10:53:53 +0900

    [level unrated] Title: 세로 읽기, Time: 0.03 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181904. 세로 읽기/README.md
+++ 프로그래머스/unrated/181904. 세로 읽기/README.md
@@ -1,10 +1,10 @@
 # [level unrated] 세로 읽기 - 181904 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181904?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181904) 
 
 ### 성능 요약
 
-메모리: 78.3 MB, 시간: 0.05 ms
+메모리: 75.9 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 4일 10:48:55
+2023년 11월 4일 10:53:52
 
 ### 문제 설명
 
--- 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.java
+++ 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.java
@@ -1,21 +1,31 @@
 class Solution {
     public String solution(String my_string, int m, int c) {
         String answer = "";
+        StringBuilder sb = new StringBuilder();
+        
+        /* 간단한 풀이 */
+        for (int i = c - 1; i < my_string.length(); i += m) {
+            sb.append(my_string.charAt(i));
+        }
+
+        answer = String.valueOf(sb);
+        
+        
+        /* (기존 풀이)
         String[] str = new String[my_string.length()/m];
         int startIdx = 0;
 
         for (int i = 0; i < str.length; i++) {
             str[i] = my_string.substring(startIdx, startIdx + m);
             startIdx += m;
-        }
-
-        StringBuilder sb = new StringBuilder();
+        }        
 
         for (int i = 0; i < str.length; i++) {
             sb.append(str[i].substring(c - 1, c));
         }
 
         answer = String.valueOf(sb);
+        */
 
         return answer;
     }
