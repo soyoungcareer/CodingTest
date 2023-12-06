e7dcefd8f22a2a9390165336b71bff0cae0a74a0
Soyoung Jeong <76424733+soyoungcareer@users.noreply.github.com>
2023-12-06 18:14:16 +0900

    [level unrated] Title: 첫 번째로 나오는 음수, Time: 0.02 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.java
+++ 프로그래머스/unrated/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.java
@@ -1,5 +1,13 @@
 class Solution {
     public int solution(int[] num_list) {
+        /* return 활용 */
+        for (int i = 0; i < num_list.length; i++) {
+            if (num_list[i] < 0) return i;
+        }
+        
+        return -1;
+        
+        /* (기존 풀이)
         int answer = 0;
 
         for (int i = 0; i < num_list.length; i++) {
@@ -14,5 +22,6 @@ class Solution {
         answer = answer == num_list.length ? -1 : answer;
 
         return answer;
+        */
     }
 }
