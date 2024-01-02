d0c425e93838da791694aa0ace63d57319cf201a
Soyoung Jeong <76424733+soyoungcareer@users.noreply.github.com>
2024-01-02 14:50:43 +0900

    [level unrated] Title: 순서 바꾸기, Time: 2.83 ms, Memory: 81.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181891. 순서 바꾸기/순서 바꾸기.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < num_list.length; i++) {
+            if (i >= n) {
+                list.add(num_list[i]);
+            }
+        }
+
+        for (int i = 0; i < num_list.length; i++) {
+            if (i < n) {
+                list.add(num_list[i]);
+            }
+        }
+
+        int[] answer = list.stream()
+                        .mapToInt(i -> i)
+                        .toArray();
+
+        return answer;
+    }
+}
